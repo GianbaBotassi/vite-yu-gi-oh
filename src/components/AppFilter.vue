3b011f8ee8b2f3b4481075c7c6cc442a897f6275
GianbaBotassi <gianba89@gmail.com>
2023-06-05 15:28:16 +0200

    Install bootstrap, create general and partial styles

---
+++ src/components/AppFilter.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: "AppFilter"
+}
+
+</script>
+
+<template>
+    <select name="type">
+        <option value="Alien">Alien</option>
+        <option value="Cighi">Cighi</option>
+        <option value="Pinni">Pinni</option>
+        <option value="Renata">Renata</option>
+    </select>
+</template>
+
+
+<style scoped lang="scss"></style>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    name: "AppHeader"
+}
+
+</script>
+
+<template>
+    <header>
+        <div>
+            <img src="/img/logo.jpg" alt="">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+
+    </header>
+</template>
+
+<style scoped lang="scss">
+@use 'styles/partials/mixins' as *;
+@use 'styles/general' as *;
+
+
+header {
+    @include flex-content(start);
+    margin-left: 20px;
+
+
+    div {
+        height: 50px;
+        width: 50px;
+    }
+}
+</style>
